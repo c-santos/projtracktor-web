c7f51e87dd4cdb7dadafc6820d79153a49cbf915
c-santos <jcms121899@gmail.com>
2024-12-07 15:48:28 +0800

    tanstack

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,14 @@
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import path from 'path';
+import { TanStackRouterVite } from '@tanstack/router-plugin/vite';
+import { resolve } from 'path';
 
 // https://vitejs.dev/config/
-export default ({ mode }: any) => {
-    const env = loadEnv(mode, process.cwd());
-
-    const PORT = parseInt(`${env.VITE_PORT ?? '3000'}`);
-
-    return defineConfig({
-        plugins: [react()],
-        server: {
-            port: PORT,
+export default defineConfig({
+    plugins: [TanStackRouterVite({}), react()],
+    resolve: {
+        alias: {
+            '@/': resolve(__dirname, 'src/'),
         },
-        resolve: {
-            alias: {
-                '@': path.resolve(__dirname, './src'),
-            },
-        },
-    });
-};
+    },
+});
