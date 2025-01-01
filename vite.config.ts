4df52b4ce1a5144e7d2c53dab57fee132ee23d86
c-santos <jcms121899@gmail.com>
2025-01-01 22:24:52 +0800

    updated vite config env var retrieval

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,22 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import { resolve } from 'path';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-    plugins: [react()],
-    resolve: {
-        alias: {
-            '@/': resolve(__dirname, 'src/'),
+export default ({ mode }: { mode: string }) => {
+    process.env = loadEnv(mode, process.cwd(), '')
+    return defineConfig({
+        plugins: [react()],
+        server: {
+            port: parseInt(process.env.VITE_PORT!) || 3000
         },
-    },
-});
+        resolve: {
+            alias: {
+                '@/': resolve(__dirname, 'src/'),
+            },
+        },
+        envDir: process.cwd(),
+        envPrefix: '',
+        keepProcessEnv: true
+    });
+}
