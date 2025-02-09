f6849effc2c1b1dcf5bf958bfc677c7742ca3da1
c-santos <jcms121899@gmail.com>
2025-02-09 23:21:18 +0800

    fixed html body margins

--- src/ui/pages/BasePage.tsx
+++ src/ui/pages/BasePage.tsx
@@ -1,16 +1,15 @@
 import { Outlet } from 'react-router-dom';
 import Navbar from '../components/Navbar';
-import { ScrollArea, Theme, ThemePanel } from '@radix-ui/themes';
+import { Theme, ThemePanel } from '@radix-ui/themes';
+import './index.css';
 
 export default function BasePage() {
     return (
         <>
             <Theme appearance='dark'>
                 <Navbar />
-                <ScrollArea>
-                    <Outlet />
-                    <ThemePanel />
-                </ScrollArea>
+                <Outlet />
+                <ThemePanel />
             </Theme>
         </>
     );
